19b8572bac65962ba9adb33204e01559ecf23fad
oljas <olja.smirnov93@gmail.com>
2023-08-20 19:51:27 +0300

    CW#25 calendar

--- src/telran/time/NextFriday13.java
+++ src/telran/time/NextFriday13.java
@@ -2,22 +2,38 @@ package telran.time;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
 public class NextFriday13 implements TemporalAdjuster {
 
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
-		
-		LocalDate nextFriday13 = (LocalDate) temporal;
-	    do {
-	    	nextFriday13 = nextFriday13.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
-	    }while(nextFriday13.getDayOfMonth() != 13);
+		if(!temporal.isSupported(ChronoField.DAY_OF_WEEK) || !temporal.isSupported(ChronoField.DAY_OF_MONTH)) {
+			throw new UnsupportedTemporalTypeException("must support DAY_OF_WEEK and DAY_OF_MONTH");
+		}
+		if(!temporal.isSupported(ChronoUnit.MONTHS)) {
+			throw new UnsupportedTemporalTypeException("must support ChronoUnit.Month");
+		}
+		temporal = adjustTemporal(temporal);
 	    
-		return nextFriday13;
+	    while(temporal.get(ChronoField.DAY_OF_WEEK) != DayOfWeek.FRIDAY.getValue()) {
+	    	temporal = temporal.plus(1, ChronoUnit.MONTHS);
+	    }
+	    
+		return temporal;
 		
+	}
+
+	private Temporal adjustTemporal(Temporal temporal) {
+		if(temporal.get(ChronoField.DAY_OF_MONTH) >= 13) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		}
+		return temporal.with(ChronoField.DAY_OF_MONTH, 13);
 	}
 
 }
